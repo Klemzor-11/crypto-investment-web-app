3ca1f3fb18c51fcedb3c4a6cd0820a3fdce59240
Klemzor-11 <kelvinklemz@gmail.com>
2026-05-22 11:02:03 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Klemzor Investment App</h1>
+      <p>Frontend is live 🚀</p>
+
+      <button onClick={() => alert("Working!")}>
+        Test Button
+      </button>
+    </div>
+  );
+}
